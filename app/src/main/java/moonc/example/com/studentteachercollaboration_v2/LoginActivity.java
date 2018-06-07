6c4ccd867d39682f36b34362767f18c746b5f392
shoukhin93 <shoukhin1993@gmail.com>
2018-06-07 14:10:40 +0600

    removed unnecessary codes in LoginActivity

--- app/src/main/java/moonc/example/com/studentteachercollaboration_v2/LoginActivity.java
+++ app/src/main/java/moonc/example/com/studentteachercollaboration_v2/LoginActivity.java
@@ -1,33 +1,13 @@
 package moonc.example.com.studentteachercollaboration_v2;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.ActionBar;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,77 +25,34 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- *
- */
-
-
-
 public class LoginActivity extends AppCompatActivity {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
     private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-
-    // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private FirebaseAuth firebaseAuth;
-    private TextView register,lost_pass;
+    private TextView register;
+    private TextView lost_pass;
+    private Button mEmailSignInButton;
     Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        spinner = (Spinner)findViewById(R.id.spn_options);
-
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-
-        mPasswordView = (EditText) findViewById(R.id.password);
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        initializeViews();
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
         });
-        mProgressView = findViewById(R.id.login_progress);
-
-        lost_pass = (TextView)findViewById(R.id.tv_lostPass);
-        register = (TextView)findViewById(R.id.tv_register);
-
 
         lost_pass.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,Working.class);
+                Intent intent = new Intent(LoginActivity.this, Working.class);
                 startActivity(intent);
             }
         });
@@ -124,66 +61,45 @@ public class LoginActivity extends AppCompatActivity {
         register.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,Working.class);
+                Intent intent = new Intent(LoginActivity.this, Working.class);
                 startActivity(intent);
             }
         });
 
     }
 
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
+    private void initializeViews() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        spinner = (Spinner) findViewById(R.id.spn_options);
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mProgressView = findViewById(R.id.login_progress);
+        lost_pass = (TextView) findViewById(R.id.tv_lostPass);
+        register = (TextView) findViewById(R.id.tv_register);
     }
 
-    /**
-     * Callback received when a permissions request has been completed.
-     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         if (requestCode == REQUEST_READ_CONTACTS) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
             }
         }
     }
 
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptLogin() {
 
         // to avoid firebase authentication,,,,
         boolean b = true;
-        if(b) {
+        if (b) {
 
             Intent intent = new Intent(LoginActivity.this, Admin.class);
             startActivity(intent);
             return;
         }
 
-        Toast.makeText(getApplicationContext(),"Stop",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Stop", Toast.LENGTH_SHORT).show();
 
         // Reset errors.
         mEmailView.setError(null);
@@ -193,91 +109,78 @@ public class LoginActivity extends AppCompatActivity {
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
 
-        if(email.isEmpty()){
+        if (email.isEmpty()) {
             mEmailView.setError("Email Field Can't be empty");
             mEmailView.requestFocus();
             return;
         }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             mEmailView.setError("Not a valid Email");
             mEmailView.requestFocus();
             return;
         }
 
-        if(password.isEmpty()){
+        if (password.isEmpty()) {
             mPasswordView.setError("Password Field Can't be empty");
             mEmailView.requestFocus();
             return;
         }
 
-        if(password.length()<8){
+        if (password.length() < 8) {
             mPasswordView.setError("Password can't be less than 8 character");
             mEmailView.requestFocus();
             return;
         }
 
-
-        String selectedItem = (String)spinner.getSelectedItem();
-
-        Toast.makeText(getApplicationContext(),selectedItem,Toast.LENGTH_SHORT).show();
-
-        if(selectedItem.equals("Admin")) {
+        String selectedItem = (String) spinner.getSelectedItem();
+        if (selectedItem.equals("Admin")) {
             firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
-                        Intent intent = new Intent(LoginActivity.this,Admin.class);
+                        Intent intent = new Intent(LoginActivity.this, Admin.class);
                         startActivity(intent);
-
                         Toast.makeText(getApplicationContext(), "Successfully logged in", Toast.LENGTH_SHORT).show();
-                    }else
-                    {
-                        Toast.makeText(getApplicationContext(),"Failed to Login",Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Failed to Login", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
-        }else if(selectedItem.equals("Student")){
-
-            Toast.makeText(getApplicationContext(),"On Student",Toast.LENGTH_SHORT).show();
-
-            LoginAsRole(email,password,selectedItem);
-        }else{
-                LoginAsRole(email,password,selectedItem);
+        } else if (selectedItem.equals("Student")) {
+            LoginAsRole(email, password, selectedItem);
+        } else {
+            LoginAsRole(email, password, selectedItem);
         }
     }
 
-
-
-    private void LoginAsRole(final String email, final String password,final String role) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("User_details");
+    private void LoginAsRole(final String email, final String password, final String role) {
+        DatabaseReference databaseReference = FirebaseDatabase
+                .getInstance().getReference("User_details");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot data:dataSnapshot.getChildren()){
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
                     Students_detail detail = data.getValue(Students_detail.class);
                     String Email = detail.getEmail_id_number();
                     String Password = detail.getPassword();
-                    String Role  = detail.getRole();
+                    String Role = detail.getRole();
 
-                    if(Email.equals(email) && Password.equals(password) && Role.equals(role)){
-                        if(Role.equals("Student")) {
+                    if (Email.equals(email) && Password.equals(password) && Role.equals(role)) {
+                        if (Role.equals("Student")) {
                             String Session = detail.getSession();
                             Intent intent = new Intent(LoginActivity.this, Student_activity.class);
-                            intent.putExtra("session",Session);
+                            intent.putExtra("session", Session);
                             startActivity(intent);
                             break;
 
-                        }else
-                        {
+                        } else {
                             Intent intent = new Intent(LoginActivity.this, Teacher_activity.class);
                             startActivity(intent);
                             break;
                         }
                     }
                 }
-                    //Toast.makeText(getApplicationContext(),"Failed to login as student",Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
